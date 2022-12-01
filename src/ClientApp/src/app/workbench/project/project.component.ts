8521a6d9640d4d9cadc024e647a2721b000efe7f
LiphiTC <iprogcen@hotmail.com>
2022-12-01 13:49:41 +0300

    updated quill

--- src/ClientApp/src/app/workbench/project/project.component.ts
+++ src/ClientApp/src/app/workbench/project/project.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
+import Quill from 'quill';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css', './project.component.sass']
 })
 export class ProjectComponent implements OnInit {
 
@@ -12,4 +14,33 @@ export class ProjectComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+
+  blurred = false
+  focused = false
+
+  created(event: any) {
+    // tslint:disable-next-line:no-console
+    console.log('editor-created', event)
+  }
+
+  changedEditor(event: any) {
+    // tslint:disable-next-line:no-console
+    console.log('editor-change', event)
+  }
+
+  focus($event : any) {
+    // tslint:disable-next-line:no-console
+    console.log('focus', $event)
+    this.focused = true
+    this.blurred = false
+  }
+
+  blur($event : any) {
+    // tslint:disable-next-line:no-console
+    console.log('blur', $event)
+    this.focused = false
+    this.blurred = true
+  }
+  
 }
